a583ddc8e72710ea95406bdc74c2c6615da02c3b
Denis <dkihada@gmail.com>
2021-02-01 11:25:05 +0500

    Slider

--- js/script.js
+++ js/script.js
@@ -276,4 +276,55 @@ window.addEventListener('DOMContentLoaded', function () { //JS начинает 
                 .catch(() => statusMessage.innerHTML = message.failure)
                 .then(() => clearInput)
     }); 
+
+    //Slider
+
+    let slideIndex = 1, //Параметр текущего слайда
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex); //Вызывает функцию, которая будет выполняться ниже
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none'); //Перебирает все слайды и скрывает их
+        // for (let i = 0; i < slides.length; i++) { //Тот же перебор, только другой вариант написания
+        //     slides[i].style.display = 'none'
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active')); //Перебирает все полученные элементы и удалеяет указанный класс
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
 }); 
